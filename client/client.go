cbace2ff8879151f92612c6bd1d30f8f52a1819f
tsujio <ntsujio@gmail.com>
2021-08-11 22:22:27 +0900

    Add client

---
+++ client/client.go
@@ -0,0 +1,37 @@
+package client
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+const (
+	endpoint = "https://game-logging-server.tsujio.org/log"
+)
+
+func Log(gameName string, payload interface{}) error {
+	p, err := json.Marshal(payload)
+	if err != nil {
+		return err
+	}
+	b, err := json.Marshal(map[string]interface{}{
+		"game_name": gameName,
+		"payload":   p,
+	})
+	if err != nil {
+		return err
+	}
+	body := bytes.NewReader(b)
+
+	resp, err := http.Post(endpoint, "application/json", body)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("Response from game logging server: %s", resp.Status)
+	}
+
+	return nil
+}
---
+++ client/go.mod
@@ -0,0 +1,3 @@
+module github.com/tsujio/game-logging-server/client
+
+go 1.16
